d75129447490939dc8167e02c121802897f7f532
Logan Markewich <logan.markewich@live.com>
2026-03-16 21:55:51 -0600

    diff base url

--- docs.config.mjs
+++ docs.config.mjs
@@ -2,10 +2,10 @@ export default {
   section: "liteparse",
   label: "LiteParse",
   content: [
-    { src: "./docs/src/content/docs/liteparse", dest: "python/liteparse" },
+    { src: "./docs/src/content/docs/liteparse", dest: "liteparse" },
   ],
   sidebar: [{
     label: "LiteParse",
-    content: { type: "autogenerate", directory: "python/liteparse", collapsed: true },
+    content: { type: "autogenerate", directory: "liteparse", collapsed: true },
   }],
 };
